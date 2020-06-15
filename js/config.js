9f12be840e566ceceda80780ee87ea1934478c35
jklswr <marcplansr@gmail.com>
2020-06-15 13:40:14 +0200

    config update

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'eu-central-1_V9hZlRHUv', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '8ipua604b8u0uu4j416gtcds4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-central-1' // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
 };
