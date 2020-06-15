14f2e26bc1a1f3d03784decb97d9eea3f58b4d49
jklswr <marcplansr@gmail.com>
2020-06-15 19:53:04 +0200

    added api invoke url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: 'eu-central-1' // e.g. us-east-2
   },
   api: {
-    invokeUrl: 'eyJraWQiOiJyS1ppUnQ2QUdsWUlxRktBUTZ1V0xCRmR4aWpoNFZLS29xYVwvYVFzM1pKST0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI3ZDJhZmNkYi00MDE4LTQ2YjYtODEyYy0xMWE1ZjdhYjBhN2QiLCJhdWQiOiI4aXB1YTYwNGI4dTB1dTRqNDE2Z3RjZHM0IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV2ZW50X2lkIjoiMDYwNmFiM2QtZWJkOS00OGJlLWJlYzUtYzZkZTI5OTA4NTczIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1OTIyMjE2NDAsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS1jZW50cmFsLTEuYW1hem9uYXdzLmNvbVwvZXUtY2VudHJhbC0xX1Y5aFpsUkhVdiIsImNvZ25pdG86dXNlcm5hbWUiOiJ1c2VybG9naW4wMS1hdC1wcm90b25tYWlsLmNvbSIsImV4cCI6MTU5MjIyNTI0MCwiaWF0IjoxNTkyMjIxNjQwLCJlbWFpbCI6InVzZXJsb2dpbjAxQHByb3Rvbm1haWwuY29tIn0.hfFG4276XqMO7pzCuoA4cMcfolh-UaI0_C620O7qgHbkuiWmKaW0cVAgaETA8TrfkrYLST54r6kT8HHtkTa1Nf6FXiUvxyUClNBmkFsI4DgfUT88mk9-BCItxVr-uO9vTJXuEAVgE7WJr9YdzkaTT_CQE31kgUjCov9c-TfeBSnkBdCpLDwtuMZVNf5CRtdRwowsFs3NY7lLc__cDODyXNCusDpW-wNwJrSXKfyBdpp8sR99OWfPUQW0jb079OGR4HuERIFTCq-y5WEB_t43LKqZh9ubxOjFRLAHN2umF7XJhLKG0HkO5l0JHq7MxrwojhCqYTp47HlEVBK9uf465g' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: 'https://7p7wkxlqr4.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   }
 };
